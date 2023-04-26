bd133c58c9b197985de2733e39fd6bf198d2a6ec
Nikita <fiwile@gmail.com>
2023-04-26 18:21:04 +0300

    added bogo sort

---
+++ sorting_algorithms/bogo_sort.py
@@ -0,0 +1,17 @@
+import random
+
+
+# Don't use it
+def is_sorted(values):
+    for index in range(len(values) - 1):
+        if values[index] > values[index + 1]:
+            return False
+
+    return True
+
+
+def bogo_sort(values):
+    while not is_sorted(values):
+        random.shuffle(values)
+
+    return values
